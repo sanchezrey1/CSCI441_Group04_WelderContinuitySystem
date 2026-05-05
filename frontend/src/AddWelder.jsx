56538db6aed864ff9a9a153e94f0b6b9ed4a5582
Rachel White <r_white4@mail.fhsu.edu>
2026-05-05 17:52:46 -0500

    UI fix

--- frontend/src/AddWelder.jsx
+++ frontend/src/AddWelder.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Sidebar } from "./Dashboard";
 import "./Dashboard.css";
-//import "./AddWelder.css";
 
 const API_BASE = "http://localhost:8000/api";
 
@@ -12,23 +11,16 @@ export default function AddWelder() {
         last_name: "",
         department: "",
         hire_date: "",
-        qualifications: [
-            {
-                proccess_id: 1,
-                code_id: 1,
-                qualified_date: "",
-                expiration_date: ""
-            }
-        ]
+        qualifications: ""
     });
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
-        updated[0][field] = value;
-        setFormData({ ...formData, qualifications: updated });
     };
 
     const handleSubmit = async () => {
+        e. preventDefault();
+
         try {
             const res = await fetch(`${API_BASE}/welders/full`, {
                 method: "POST",
@@ -62,30 +54,38 @@ export default function AddWelder() {
                         />
                 </div>
 
-                <div className="topbar-right"></div>
+                <div className="topbar-right">Admin</div>
             </div>
 
             {/* page body */}
-            <div className="register-wrapper">
-                <div className="register-card">
-                    <h2 className="register-title">Welder Registration</h2>
+            <div className="page-body">
 
-                    <form onSubmit={handleSubmit} className="register-form">
+            <div className="form-card">
+                <div className="form-title">
+                    Welder Registration
+                    </div>
+
+                    <form onSubmit={handleSubmit} className="form-group">
                         
+                        <div className="form-group">
                         <label>Employee ID</label>
                         <input
                             name="employee_id"
                             placeholder="00067682"
                             onChange={handleChange}
                         />
+                        </div>
 
+                        <div className="form-group">
                         <label>Name</label>
                         <input
                             name="full_name"
                             placeholder="Full Name"
                             onChange={handleChange}
                         />
+                        </div>
 
+                        <div className="form-group">
                         <label>Department</label>
                         <select
                             name="department"
@@ -96,31 +96,38 @@ export default function AddWelder() {
                             <option>Fabrication</option>
                             <option>QA</option>
                         </select>
+                        </div>
 
+                        <div className="form-group">
                         <label>Hire Date</label>
                         <input
                             type="date"
                             name="hire_date"
                             onChange={handleChange}
                         />
+                        </div>
 
+                        <div className="form-group">
                         <label>Role</label>
                         <select
                             name="role"
                             onChange={handleChange}
                         >
                             <option value="">Select a Role</option>
-                            <option>Weldere</option>
+                            <option>Welder</option>
                             <option>Supervisor</option>
                             <option>Inspector</option>
                         </select>
+                        </div>
 
+                        <div className="form-group">
                         <label>Qualifications</label>
                         <input
                             name="qualifications"
                             placeholder="Select or Add All Qualification(s)"
                             onChange={handleChange}
                         />
+                        </div>
 
                         <button type="submit" className="add-record-btn">
                             Add Record
